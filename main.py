c558c941e4a6ecadb30cda8097735fee53075f3a
Conrad Coyanda-Parkzes <conradnexus@gmail.com>
2024-07-10 15:10:35 -0400

    created new branch to work in vscode directly

--- main.py
+++ main.py
@@ -1,64 +1,36 @@
+from flask import Flask, render_template, url_for, redirect, request
 import requests
-import pandas as pd
-import sqlalchemy as db
-import tmdbsimple as tmdb
 
-extracted_data = []
+app = Flask(__name__)
+TMBD_API_KEY = 'f506f78c7f61db273279624ae6df02d9'
 
-tmdb.API_KEY = 'f506f78c7f61db273279624ae6df02d9'
+@app.route('/')
+# search / landing page to display top trending media, home page
+def landing():
+    return render_template('search.html')
 
-def get_movies(page):
-    search = tmdb.Movies()
-    response = search.popular(page=page)
-    return response['results']
+@app.route('/search', methods=['GET'])
+def search():
+    keyword = request.args.get('keyword')
+    if not keyword:
+        return "No keyword provided."
+    return redirect(url_for('search_results', keyword=keyword))
 
-def get_tv_shows(page):
-    search = tmdb.TV()
-    response = search.popular(page=page)
-    return response['results']
+@app.route('/search_results/<keyword>')
+def search_results(keyword):
+    response = requests.get(
+        f'https://api.themoviedb.org/3/search/multi?api_key={TMBD_API_KEY}&query={keyword}'
+    )
 
-# stores movie and tv show data in a list of dictionaries
-def get_data(items, item_type):
-    global extracted_data
-    for item in items:
-        if item_type == 'movie':
-            title = item.get('title')
+    if response.status_code == 200:
+        data = response.json()
+        if 'results' in data:
+            results = data.get('results', [])
         else:
-            title = item.get('name')
-        poster_path = f"https://image.tmdb.org/t/p/w500{item.get('poster_path')}"
-        rating = item.get('vote_average')
-        extracted_data.append({
-            'title': title,
-            'poster_path': poster_path,
-            'rating': rating,
-            'type': item_type
-        })
-    return extracted_data
-
-def make_table(data):
-    # error handling
-    if not extracted_data:
-        print("Unable to create table.")
+            results = []
+        return render_template('results.html', keyword=keyword, results=results)
     else:
-        dataf = pd.DataFrame(extracted_data)
-        engine = db.create_engine('sqlite:///tmdb_data.db')
-        dataf.to_sql('all_media', con=engine, if_exists='replace', index=False)
-        with engine.connect() as connection:
-            query_result = (
-                connection.execute(db.text("SELECT * FROM all_media;"))
-                .fetchall()
-            )
-            print(pd.DataFrame(query_result))
-
-def main():
-    all_data = []
-    for page in range(1, 6):  # sample range - first 5 pages
-        movies = get_movies(page)
-        tv_shows = get_tv_shows(page)
-        all_data.extend(get_data(movies, 'movie'))
-        all_data.extend(get_data(tv_shows, 'tv_show'))
-    
-    make_table(all_data)
+        return f"Error: {response.status_code} - {response.text}"
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=True)
